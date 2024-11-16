fbe03f3490504b0e4689c925842da482838d52fb
rgb234 <fehs0611@gmail.com>
2024-11-16 15:05:34 +0900

    train results

--- train/train.py
+++ train/train.py
@@ -5,16 +5,22 @@ settings["datasets_dir"] = "/data/fehs0611/datasets/"
 settings.update()
 
 # Load a COCO-pretrained YOLOv5n model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8m.pt")
 # model = YOLO("./v8n-pretraining-result/weights/best.pt")
 
 # Display model information (optional)
 # model.info()
 
+# memo
+# optimizer: 'optimizer=auto' found, ignoring 'lr0=0.01' and 'momentum=0.937'
+# and determining best 'optimizer', 'lr0' and 'momentum' automatically...
+# SGD(lr=0.01, momentum=0.9)
+
+
 path = "../street-facilities.yaml"
 results = model.train(
     data=path,
-    epochs=500,
+    epochs=300,
     imgsz=640,
     device=[0],  # use one cuda device
 )
